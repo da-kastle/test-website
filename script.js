7d76cf2988a159877ac6fc5458b90b589d194358
da-kastle <castlecopywriters@gmail.com>
2025-02-04 06:52:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,16 +9,6 @@ const firebaseConfig = {
   measurementId: "G-571V19Z2XR"
 };
 
-// Firebase configuration (Replace with your actual Firebase project details)
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
